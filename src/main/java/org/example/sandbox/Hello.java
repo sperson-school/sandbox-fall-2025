551bc765e476dbccb13c7deaaba25b6863ebf3e6
Seth Person <set2131308@maricopa.edu>
2025-08-29 12:41:22 -0700

    Second Commit

--- src/main/java/org/example/sandbox/Hello.java
+++ src/main/java/org/example/sandbox/Hello.java
@@ -3,5 +3,6 @@ package org.example.sandbox;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is a sandbox environment.");
     }
 }
